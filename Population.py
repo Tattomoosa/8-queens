e7c228d9e349f58ebe2cd472b365200cf5bd9229
Matt <mlotousa@gmail.com>
2021-02-21 00:53:41 -0800

    2 children per parent pair

--- Population.py
+++ Population.py
@@ -2,8 +2,6 @@ from Board import Board
 import random
 import sys
 
-PICK_PARENTS_FROM=3
-
 class Population:
 
     def __init__(self, size=10, mutation_chance=0.1):
@@ -15,31 +13,36 @@ class Population:
         Board.mutation_chance = mutation_chance
         self.generation = 0
 
-    def get_best_score(self):
-        return self.get_best().score
 
     def step(self):
         self.update_fitness()
         children = []
-        for i in range(self.size):
+        # for i in range(self.size):
+        while len(children) < self.size:
             parents = self.select_parents()
-            children.append(parents[0].breed(parents[1]))
+            children.append(parents[0] + parents[1])
+            children.append(parents[1] + parents[0])
+        while len(children) > self.size:
+            children.pop()
         self.pop = children
         self.generation += 1
+
 
     def sort(self):
         self.pop.sort(key=lambda x : x.score)
         self.pop.reverse()
+
 
     def update_fitness(self):
         fitness_sum = 0
         for pop in self.pop:
             fitness_sum += pop.score
         for pop in self.pop:
-            # on low population sizes, every board can wind up with a fitness of
+            # on low population sizes, rarely every board can wind up with a fitness of
             # 0. seems like maybe that shouldn't happen, but since it does let's
             # stop it.
             pop.fitness = (pop.score / fitness_sum) if fitness_sum != 0 else 0
+
 
     def select_parents(self):
         parent0, parent1 = None, None
@@ -59,12 +62,14 @@ class Population:
 
         return (parent0, parent1)
 
+
     def print(self):
         # clear screen
         print(chr(27) + "[2J")
         print(f'GENERATION: {self.generation}')
         print(f'BEST: {self.get_best_score()}')
         print(f'AVG: {self.get_avg_score()}')
+
 
     def print_all(self):
         self.print()
@@ -74,10 +79,12 @@ class Population:
             Board.printn(self.pop[i:i+n])
             i += n
 
+
     def print_best(self):
         self.print()
         print('BEST:')
         self.get_best().print()
+
 
     def get_best(self):
         best = self.pop[0]
@@ -86,11 +93,17 @@ class Population:
                 best = pop
         return best
 
+
+    def get_best_score(self):
+        return self.get_best().score
+
+
     def get_avg_score(self):
         n = 0
         for pop in self.pop:
             n += pop.score
         return n / len(self.pop)
+
 
     def contains_winner(self):
         for p in self.pop:
